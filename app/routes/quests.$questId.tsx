0e27e529fc4d16f301e58376e446eb06c8acf839
zakariyarezok <rezok.zakariya@gmail.com>
2023-03-07 19:19:06 +0100

    quest layout

--- app/routes/quests.$questId.tsx
+++ app/routes/quests.$questId.tsx
@@ -1,4 +1,5 @@
 import { getQuestById } from "@/api/get-quest";
+import { CustomCheckbox } from "@/components/atoms/checkbox";
 import { getUserSession } from "@/session.server";
 import { useLoaderData } from "@remix-run/react";
 import type { LoaderArgs } from "@remix-run/server-runtime";
@@ -15,5 +16,45 @@ export const loader = async ({ request, params }: LoaderArgs) => {
 export default function Quest() {
   const data = useLoaderData<typeof loader>();
   console.log(data);
-  return <div>{data.title}</div>;
+  return (
+    <div>
+      <h2 className="mb-14 text-[24px]">{data.title}</h2>
+      <div className="w-[60%]">
+        <h3 className="mb-5 border-b-[1px] border-[#363636] pb-[6px] text-[14px] text-[#9F9F9F]">
+          Description
+        </h3>
+        <div className="w-[80%] text-[14px] italic leading-[22px] text-[#C8C6E4]">
+          {data.description}
+        </div>
+      </div>
+      <div className="mt-14 w-[60%]">
+        <h3 className="mb-5 border-b-[1px] border-[#363636] pb-[6px] text-[14px] text-[#9F9F9F]">
+          Objectifs
+        </h3>
+        <div className="flex w-[80%] flex-col gap-2 text-[14px]">
+          {data.tasks.map((task: { id: string; title: string }) => (
+            <div className="flex items-center gap-[10px]" key={task.id}>
+              <CustomCheckbox id={`box${task.title}`}></CustomCheckbox>
+              <label className="cursor-pointer" htmlFor={`box${task.title}`}>
+                {task.title}
+              </label>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="mt-14 w-[60%]">
+        <h3 className="mb-5 border-b-[1px] border-[#363636] pb-[6px] text-[14px] text-[#9F9F9F]">
+          Récompenses
+        </h3>
+        <div className="flex w-[80%] flex-col gap-2 text-[14px]">
+          <p>
+            {data.xp} <span className="font-bold text-[#69D0F1]">XP</span>
+          </p>
+          <p>
+            {data.gold} <span className="font-bold text-[#E4BC2F]">Gold</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
